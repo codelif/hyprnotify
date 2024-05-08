ab4b8ecbeeb88184f3473ccf71488f9b9872049a
Harsh Sharma <68972644+codelif@users.noreply.github.com>
2024-05-08 13:45:14 +0530

    (hypripc): prepare for v40 socket path change

--- internal/hypripc.go
+++ internal/hypripc.go
@@ -4,20 +4,39 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"path"
 	"strconv"
 	"strings"
 )
 
 func GetHyprSocketAddr() string {
-	env := os.Getenv("HYPRLAND_INSTANCE_SIGNATURE")
-	if env == "" {
+	instance_signature := os.Getenv("HYPRLAND_INSTANCE_SIGNATURE")
+	if instance_signature == "" {
 		fmt.Println("Hyprland is not running!")
 		os.Exit(1)
 	}
 
-	addr := "/tmp/hypr/" + env + "/.socket.sock"
+	runtime_dir := os.Getenv("XDG_RUNTIME_DIR")
+	if runtime_dir == "" {
+		runtime_dir = "/run/user/1000" // try for first user
+	}
 
-	return addr
+	socket_addr := path.Join(runtime_dir, "/hypr", instance_signature, "/.socket.sock")
+
+	if _, err := os.Stat(socket_addr); err == nil {
+		return socket_addr
+	}
+
+	// try pre v40 socket path
+	socket_addr = path.Join("/tmp/hypr/", instance_signature, "/.socket.sock")
+
+	if _, err := os.Stat(socket_addr); err == nil {
+		return socket_addr
+	}
+
+	fmt.Println("Hyprland IPC path is not available!")
+	os.Exit(1)
+	return ""
 }
 
 type HyprConn struct {
@@ -39,7 +58,6 @@ func (hypr HyprConn) HyprCtl(args ...string) {
 }
 
 func (hypr HyprConn) SendNotification(nf *Notification) {
-
 	icon := i32ToString(nf.icon.value)
 	timeout := i32ToString(nf.time_ms)
 	font_size := i32ToString(nf.font_size.value)
